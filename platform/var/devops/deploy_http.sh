0d3c553d018f3b2498c081c53ecdba9c2c1f8b6e
Brian Seymour <imbrianseymour@gmail.com>
2018-09-01 20:46:44 -0500

    path

--- platform/var/devops/deploy_http.sh
+++ platform/var/devops/deploy_http.sh
@@ -5,10 +5,10 @@ PATH=/root/deps/nod8/bin:$PATH
 cd /net/engineerman/platform
 
 git pull
-systemctl stop engineerman-platform
+systemctl stop engineerman
 cd migrations
 bmig migrate
 cd ..
 npm install
 rm -f package-lock.json
-systemctl start engineerman-platform
+systemctl start engineerman
