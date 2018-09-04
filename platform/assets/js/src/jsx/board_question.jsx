c7a687a9f685621b80d22845e7b8bee7075ce597
Brian Seymour <imbrianseymour@gmail.com>
2018-09-04 09:42:02 -0500

    appearance

--- platform/assets/js/src/jsx/board_question.jsx
+++ platform/assets/js/src/jsx/board_question.jsx
@@ -5,29 +5,24 @@ class BoardQuestion extends React.Component {
             <a href={this.props.question.url} class="question">
                 <div class="stat_box">
                     <div>
-                        Score
-                        <div class="score">{this.props.question.score}</div>
+                        <span>Score</span>
+                        <span class="score">{this.props.question.score}</span>
                     </div>
-                    {' '}
                     <div>
-                        Views
-                        <div class="views">{this.props.question.views}</div>
-                    </div>
-                    {' '}
-                    <div>
-                        Replies
-                        <div class="comments">{this.props.question.comments}</div>
+                        <span>Replies</span>
+                        <span class="comments">{this.props.question.comments}</span>
                     </div>
                 </div>
                 <div class="content">
                     <div>
+                        <span class="title">{this.props.question.title}</span>
                         <div class="posted">
-                            <img src={this.props.question.user.avatar_url} />
+                            by
+                            {' '}
                             <a href={'/@' + this.props.question.user.username}>
                                 {this.props.question.user.display_name}
                             </a> {this.props.question.time_ago === 'now' ? 'just now' : this.props.question.time_ago + ' ago'}
                         </div>
-                        <span class="title">{this.props.question.title}</span>
                     </div>
                     <div class="tag_list">
                         <Tags tags={this.props.question.tags} />
