0f16530df8364cada0bc628f0ac427767383730d
Brian Seymour <imbrianseymour@gmail.com>
2018-11-12 20:34:33 -0600

    add third challenge

---
+++ platform/migrations/migrations/20181111035714-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+insert into challenges values (3, 1, 10, '3_list_sum', 'Sum a list of numbers', 'Take a string of comma separated numbers and print out the sum.');
+
+down:
+delete from challenges where challenge_id = 3;
