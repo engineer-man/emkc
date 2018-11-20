4c6248236329bbb8c34ec60789a882c5054b12c9
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2018-11-20 00:52:26 -0500

    Completed initial activity feed with user posted questions.

--- platform/resources/jsx/user_activity.jsx
+++ platform/resources/jsx/user_activity.jsx
@@ -2,27 +2,25 @@ class UserActivity extends React.Component {
 
     render() {
         return (
-            <a href={this.props.question.url} class="question">
-                <div class="stat_box">
-                    <div>
-                        <span>Score</span>
-                        <span class="score">{this.props.question.score}</span>
+            <a href={this.props.question.url} class="user_activity">
+                <div class="user_activity_box">
+                    <h4 class="title">{this.props.question.title}</h4>
+                    <div class="user_activity_row">
+                        <span>Reply Count:</span>
+                        <span class="user_activity_comments">{this.props.question.comments}</span>
                     </div>
-                    <div>
-                        <span>Replies</span>
-                        <span class="comments">{this.props.question.comments}</span>
+                    <div class="user_activity_row">
+                        <span>Views:</span>
+                        <span class="user_activity_view">{this.props.question.views}</span>
                     </div>
-                </div>
-                <div class="content">
-                    <div class="title_wrapper">
-                        <span class="title">{this.props.question.title}</span>
-                        <div class="posted">
-                            by
-                            {' '}
-                            <a href={'/@' + this.props.username}>
-                                {this.props.username}
-                            </a> {this.props.question.time_ago === 'now' ? 'just now' : this.props.question.time_ago + ' ago'}
-                        </div>
+                    <div class="user_activity_row">
+                        <span>Votes:</span>
+                        <span class="user_activity_score">{this.props.question.score}</span>
+                    </div>
+                    <div class="user_activity_row">
+                        Posted:
+                        {' '}
+                        {this.props.question.time_ago === 'now' ? 'just now' : this.props.question.time_ago + ' ago'}
                     </div>
                 </div>
             </a>
