cc24091db03ebdb0a2fea185054bc0b8a00b55b9
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2018-12-24 13:39:50 -0500

    Updated view of comments.

--- platform/resources/jsx/user_comment.jsx
+++ platform/resources/jsx/user_comment.jsx
@@ -12,11 +12,13 @@ class UserComment extends React.Component {
     }
 
     render() {
-        //console.log(this.props.comment);
+        console.log(this.props);
         return (
             <a href={this.props.comment.question.url} class="user_comment">
                 <div class="user_comment_box">
-                    <span>{this.props.comment.question.title}</span>
+                    <div class="user_question_title">
+                        <span>{ this.props.username } commented on {this.props.comment.question.title}</span>
+                    </div>
                     <div>
                         <div
                             class="user_comment_comment"
@@ -25,10 +27,6 @@ class UserComment extends React.Component {
                     <div class="user_comment_row">
                         <span>Score:</span>
                         <span class="user_comment_view">{this.props.comment.score}</span>
-                    </div>
-                    <div class="user_comment_row">
-                        <span>Depth:</span>
-                        <span class="user_comment_score">{this.props.comment.depth}</span>
                     </div>
                 </div>
             </a>
