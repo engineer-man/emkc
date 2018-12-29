1a1ad68ae0d88d3b2333261ac4893cd8d1e93fc9
Shreyas Gupta <technology.shreyas@gmail.com>
2018-12-29 21:18:44 +0530

    changed from challenge id 6 to 7

--- platform/migrations/migrations/20181229135021-new-challenge.sql
+++ platform/migrations/migrations/20181229135021-new-challenge.sql
@@ -1,5 +1,5 @@
 up:
-insert into challenges values (6, 3, 50, '6_check_permutation', 'Check if one string is a permutation of another string of equal length', 'Given two strings of equal length, separated by a comma, check whether the second string is a permutation of the first string. Print "Yes" if it is, "No" if not.');
+insert into challenges values (7, 3, 50, '7_check_permutation', 'Check if one string is a permutation of another string of equal length', 'Given two strings of equal length, separated by a comma, check whether the second string is a permutation of the first string. Print "Yes" if it is, "No" if not.');
 
 down:
-delete from challenges where challenge_id = 6;
+delete from challenges where challenge_id = 7;
