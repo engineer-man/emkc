c6b6cc8806f21959ebed5810622b7f3bca2a944b
Brian Seymour <imbrianseymour@gmail.com>
2019-06-23 00:59:45 -0500

    update discord id to string

--- platform/migrations/migrations/20190622224428-discord-id-chat-messages.sql
+++ platform/migrations/migrations/20190622224428-discord-id-chat-messages.sql
@@ -1,5 +1,5 @@
 up:
-alter table discord_chat_messages add discord_id bigint unsigned not null after user;
+alter table discord_chat_messages add discord_id varchar(32) not null after user;
 alter table discord_chat_messages add index discord_id (discord_id);
 
 down:
