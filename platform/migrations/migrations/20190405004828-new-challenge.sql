97176489e9817fb1a2a599e371206f3843a20a9f
Brian Seymour <imbrianseymour@gmail.com>
2019-06-24 13:28:41 -0500

    add pig latin challenge difficulty change to old mig

--- platform/migrations/migrations/20190405004828-new-challenge.sql
+++ platform/migrations/migrations/20190405004828-new-challenge.sql
@@ -1,5 +1,5 @@
 up:
-insert into challenges values (10, 1, 10, '10_pig_latin', 'Pig latin translator', 'Take a sentence/phrase in pig latin, translate it to English.');
+insert into challenges values (10, 3, 50, '10_pig_latin', 'Pig latin translator', 'Take a sentence/phrase in pig latin, translate it to English.');
 
 down:
 delete from challenges where challenge_id = 10;
--- platform/migrations/migrations/20190416-new-difficulty-piglatin.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `challenges` SET `difficulty`=2 WHERE challenge_id=10
