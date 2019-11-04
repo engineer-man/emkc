e306cb3425a3573fcf5f54b406b223bec785780a
Brian Seymour <imbrianseymour@gmail.com>
2019-11-03 21:51:05 -0600

    reintroduce question and answer

--- platform/api/controllers/BoardController.js
+++ platform/api/controllers/BoardController.js
@@ -1,31 +1,43 @@
 module.exports = {
 
-    home(req, res) {
-        return Promise.resolve(null)
-            .then(() => {
-                return [
-                    db.users
-                        .find_all({
-                            where: {
-                                is_staff: {
-                                    $ne: 1
-                                }
-                            },
-                            order: [
-                                ['score', 'desc'],
-                                ['user_id']
-                            ],
-                            limit: 100
-                        })
-                ];
-            })
-            .spread((users) => {
-                return res.view({
-                    users
-                });
+    async home(req, res) {
+        let questions = await db.questions
+            .find_all({
+                include: [
+                    {
+                        model: db.users,
+                        as: 'user'
+                    },
+                    {
+                        model: db.tags,
+                        as: 'tags'
+                    }
+                ],
+                order: [
+                    ['question_id', 'desc']
+                ],
+                limit: 25
             });
-    },
 
-    _config: {}
+        return res.view({
+            questions: questions.map(q => {
+                return {
+                    title: q.title,
+                    score: q.score,
+                    views: q.views,
+                    comments: q.comments,
+                    url: q.url,
+                    time_ago: q.time_ago,
+                    user: {
+                        display_name: q.user.display_name,
+                        username: q.user.username,
+                        avatar_url: constant.gcloud_base_url + q.user.avatar_url,
+                        score: q.user.score
+                    },
+                    tags: q.tags
+                }
+            })
+        });
+    }
 
 };
--- platform/api/controllers/HomeController.js
+++ platform/api/controllers/HomeController.js
@@ -4,7 +4,29 @@ const q = require('q');
 module.exports = {
 
     home(req, res) {
-        return res.view();
+        return Promise.resolve(null)
+            .then(() => {
+                return [
+                    db.users
+                        .find_all({
+                            where: {
+                                is_staff: {
+                                    $ne: 1
+                                }
+                            },
+                            order: [
+                                ['score', 'desc'],
+                                ['user_id']
+                            ],
+                            limit: 100
+                        })
+                ];
+            })
+            .spread((users) => {
+                return res.view({
+                    users
+                });
+            });
     },
 
     privacy(req, res) {
