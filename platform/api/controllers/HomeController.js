5a5a1e0f19be438c5930f9791115d215bf3d63c1
Brian Seymour <imbrianseymour@gmail.com>
2020-06-02 01:38:59 -0500

    include staff on leaderboard again

--- platform/api/controllers/HomeController.js
+++ platform/api/controllers/HomeController.js
@@ -3,11 +3,6 @@ module.exports = {
     async home(req, res) {
         let users = await db.users
             .find_all({
-                where: {
-                    is_staff: {
-                        $ne: 1
-                    }
-                },
                 order: [
                     ['score', 'desc'],
                     ['user_id']
