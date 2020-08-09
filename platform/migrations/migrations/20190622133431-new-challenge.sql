c380e9da9babf9a86d6300311d7cbdf09f1b8c4e
tamatoo <tatamatoo@openviza.com>
2020-08-09 16:58:26 -0700

    Added new challenge 15_binary_cookie

---
+++ platform/migrations/migrations/20190622133431-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+insert into challenges values (15, 1, 10, '15_binary_cookie', 'Find The Cookie', 'Given a binary string, a cookie, find its place in the binary cookie jar.');
+
+down:
+delete from challenges where challenge_id = 15;
