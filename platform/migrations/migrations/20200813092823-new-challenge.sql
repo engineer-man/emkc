3bc7ecfdca4556b7db0c905c557438bec9ca1c34
Kenneth Lee <Ken.Lee@KennethLee.org>
2020-08-13 11:26:23 -0600

    sequelize code for up and down to add a 10 point question called Spiral Sum.

---
+++ platform/migrations/migrations/20200813092823-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+insert into challenges values (16, 1, 10, '16_spiral_corner', 'Spiral Sum', 'Given the size of a spiral, calcuale the sum of the four corners.');
+
+down:
+delete from challenges where challenge_id = 16;
