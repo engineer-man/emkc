10258e0094934dbebcf944d4d81ac2cb2e3fbc81
Sachin <69356296+Shivansh-007@users.noreply.github.com>
2020-08-29 09:37:47 +0530

    Create 20200829-new-challenge.sql

---
+++ platform/migrations/migrations/20200829-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+update challenges set description = 'Given a sentence, create a function which shifts the first letter of each word to the next word in the sentence (shifting right)' where challenge_id = 19;
+
+down:
+update challenges set description = 'Given a sentence, create a function which shifts the first letter of each word to the next word in the sentence (shifting right)' where challenge_id = 19;
