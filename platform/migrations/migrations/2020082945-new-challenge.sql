898ee44db6fb662142a9f1cbbefec181bd59e7e8
Sachin <69356296+Shivansh-007@users.noreply.github.com>
2020-08-29 09:34:45 +0530

    Create 2020082945-new-challenge.sql

---
+++ platform/migrations/migrations/2020082945-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+update challenges set description = 'Create a function that takes a string containing both name and number of animals and plants that may or may not be found in Chitwan National Park' where challenge_id = 19;
+
+down:
+update challenges set description = 'Create a function that takes a string containing both name and number of animals and plants that may or may not be found in Chitwan National Park' where challenge_id = 19;
