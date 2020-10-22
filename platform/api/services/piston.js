5da4430ea8150f5a67b2efccd5a8de62aca3dd11
Omar Brikaa <brikaaomar@gmail.com>
2020-10-22 12:41:28 +0200

    simplify piston execution delay

--- platform/api/services/piston.js
+++ platform/api/services/piston.js
@@ -15,41 +15,33 @@ module.exports = {
         java: 'java'
     },
 
-    // For challenges
     async execute(language, source, args) {
         if (!Array.is_array(args)) args = [args];
-
         args = args.map(arg => '' + arg);
+        const timeout = ms => new Promise(res => set_timeout(res, ms));
+        try {
+            await timeout(constant.is_prod() ? 0 : 1500);  // Delay by 1.5 seconds when using the public api
+            let result = await request
+                ({
+                    method: 'post',
+                    url: constant.is_prod()
+                        ? 'http://' + sails.config.piston.host + '/execute'
+                        : 'https://emkc.org/api/v1/piston/execute',
+                    body: {
+                        language,
+                        source,
+                        args
+                    },
+                    json: true,
+                    simple: true
+                });
 
-        // Limit api calls to 1 call each 1.5 seconds when using the public api
-        timeout = constant.is_prod() ? 0 : 1500;
-        var promise = new Promise(function(resolve, reject) {
-            setTimeout(async () => {
-                try {
-                    let result = await request
-                        ({
-                            method: 'post',
-                            url: constant.get_piston_url() + "/execute",
-                            body: {
-                                language,
-                                source,
-                                args
-                            },
-                            json: true,
-                            simple: true
-                        });
-
-                    resolve(
-                        typeof result.output === 'string'
-                            ? result.output.slice(0, 1024)
-                            : ''
-                    );
-                } catch(e) {
-                    resolve('');
-                }
-            }, timeout);
-        });
-        return promise;
+            return typeof result.output === 'string'
+                ? result.output.slice(0, 1024)
+                : '';
+        } catch(e) {
+            return '';
+        }
     }
 
 };
