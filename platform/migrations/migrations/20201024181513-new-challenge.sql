3830bde8fcecf403647874066be48ae18651d0b7
Omar Brikaa <brikaaomar@gmail.com>
2020-10-24 21:08:33 +0200

    Added a hard challenge

---
+++ platform/migrations/migrations/20201024181513-new-challenge.sql
@@ -0,0 +1,5 @@
+up:
+insert into challenges values (18, 3, 50, '18_rank_of_a_list', 'Rank of a list', 'Given a list perform some operations to calculate its rank.');
+
+down:
+delete from challenges where challenge_id = 18;
