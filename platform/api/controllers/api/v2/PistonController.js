86193d294da88bc85ec7b48f238312d74e6869ec
Brikaa <brikaaomar@gmail.com>
2021-09-30 21:17:54 +0200

    Added ratelimit headers

--- platform/api/controllers/api/v2/PistonController.js
+++ platform/api/controllers/api/v2/PistonController.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const moment = require('moment')
 const Redis = require('ioredis');
 
 module.exports = {
@@ -32,9 +32,15 @@ module.exports = {
 
         const ip = req.headers['x-real-ip'];
         const authorization = req.headers['authorization'];
+        let rate_limit_reset = '0';
+        let rate_limit_remaining = 'Infinity';
+        let rate_limit_limit = 'Infinity'
 
         if (!sails.config.piston.unlimited_keys.includes(authorization)) {
             const redis = new Redis(6379, 'redis');
+            const time_limit = 200;
+            rate_limit_remaining = '0';
+            rate_limit_limit = '1';
 
             let entry = await redis.get(`piston-${req.ip}`);
 
@@ -43,11 +49,17 @@ module.exports = {
 
                 return res
                     .status(429)
+                    .set({
+                        'x-ratelimit-limit': rate_limit_limit,
+                        'x-ratelimit-reset': entry,
+                        'x-ratelimit-remaining': rate_limit_remaining
+                    })
                     .send({
-                        message: 'Requests limited to 1 per 200ms'
+                        message: `Requests limited to 1 per ${time_limit} ms`
                     });
             } else {
-                await redis.set(`piston-${req.ip}`, 0, 'px', 200);
+                rate_limit_reset = (moment().value_of() + time_limit).to_string();
+                await redis.set(`piston-${req.ip}`, rate_limit_reset, 'px', time_limit);
             }
 
             redis.disconnect();
@@ -63,6 +75,12 @@ module.exports = {
                 user: 'Direct Usage'
             };
         }
+
+        let headers = {
+            'x-ratelimit-limit': rate_limit_limit,
+            'x-ratelimit-reset': rate_limit_reset,
+            'x-ratelimit-remaining': rate_limit_remaining
+        };
 
         try {
             let result = await piston
@@ -81,22 +99,26 @@ module.exports = {
 
             return res
                 .status(200)
+                .set(headers)
                 .send({
                     language: result.language,
                     version: result.version,
                     run: result.run,
                     compile: result.compile
                 });
+
         } catch(e) {
             if (e.status_code === 400) {
                 return res
                     .status(400)
+                    .set(headers)
                     .send({
                         message: e.message
                     });
             } else {
                 return res
                     .status(500)
+                    .set(headers)
                     .send({
                         message: 'Execution problem: ' + e.message
                     });
